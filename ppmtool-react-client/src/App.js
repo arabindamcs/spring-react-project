e48074ac1020b4750551b22bf1413dc11b56c0aa
arabindamcs <arabinda.mcs@gmail.com>
2018-11-25 16:39:55 -0500

    branch 24

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -8,6 +8,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import AddProject from "./components/Project/AddProject";
 import { Provider } from "react-redux";
 import store from "./store";
+import updateProject from "./components/Project/updateProject";
 
 class App extends Component {
   render() {
@@ -18,6 +19,7 @@ class App extends Component {
             <Header />
             <Route exact path="/dashboard" component={Dashboard} />
             <Route exact path="/addProject" component={AddProject} />
+            <Route exact path="/updateProject/:id" component={updateProject} />
           </div>
         </Router>
       </Provider>
