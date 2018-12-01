9a20478f867bc4b1abeb59b471341e742e5195e6
arabindamcs <arabinda.mcs@gmail.com>
2018-11-30 19:54:38 -0500

    branch25

--- ppmtool-react-client/src/components/Project/updateProject.js
+++ ppmtool-react-client/src/components/Project/updateProject.js
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
+import { getProject, createProject } from "../../actions/projectActions";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import classnames from "classnames";
 
-export default class UpdateProject extends Component {
+class UpdateProject extends Component {
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.getProject(id, this.props.history);
+  }
   render() {
     return (
       <div className="project">
@@ -60,3 +68,16 @@ export default class UpdateProject extends Component {
     );
   }
 }
+
+UpdateProject.propTypes = {
+  getProject: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  project: state.project.project
+});
+
+export default connect(
+  mapStateToProps,
+  { getProject, createProject }
+)(UpdateProject);
