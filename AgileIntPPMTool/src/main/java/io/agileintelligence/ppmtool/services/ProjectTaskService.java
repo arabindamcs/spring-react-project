527f18523d593ffe8f8ae3ceb8955013bfa24af4
arabindamcs <arabinda.mcs@gmail.com>
2018-12-02 23:52:02 -0500

    branch35

--- AgileIntPPMTool/src/main/java/io/agileintelligence/ppmtool/services/ProjectTaskService.java
+++ AgileIntPPMTool/src/main/java/io/agileintelligence/ppmtool/services/ProjectTaskService.java
@@ -22,25 +22,25 @@ public class ProjectTaskService {
 
         //Exceptions: Project not found
 
-        //Get the Backlog using project identifier
+        //PTs to be added to a specific project, project != null, BL exists
         Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
-
-        //Set Backlog field for ProjectTask
+        //set the bl to pt
         projectTask.setBacklog(backlog);
-
         //we want our project sequence to be like this: IDPRO-1  IDPRO-2  ...100 101
         Integer BacklogSequence = backlog.getPTSequence();
         // Update the BL SEQUENCE
         BacklogSequence++;
+        
+        backlog.setPTSequence(BacklogSequence);
 
         //Add Sequence to Project Task
-        projectTask.setProjectSequence(projectIdentifier+"-"+BacklogSequence);
-        projectTask.setProjectIdentifer(projectIdentifier);
+        projectTask.setProjectSequence(backlog.getProjectIdentifier()+"-"+BacklogSequence);
+        projectTask.setProjectIdentifier(projectIdentifier);
 
-        //INITIAL priority when priority null
-/*        if(projectTask.getPriority()==0||projectTask.getPriority()==null){
+
+        if(projectTask.getPriority()==null){ //In the future we need projectTask.getPriority()== 0 to handle the form
             projectTask.setPriority(3);
-        }*/
+        }
 
         //INITIAL status when status is null
         if(projectTask.getStatus()==""|| projectTask.getStatus()==null){
