6abba5bee2217bdd24677f8bc5a354f0ec3b4db3
arabindamcs <arabinda.mcs@gmail.com>
2018-12-11 20:59:52 -0500

    branch45

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
@@ -8,7 +7,9 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import AddProject from "./components/Project/AddProject";
 import { Provider } from "react-redux";
 import store from "./store";
-import updateProject from "./components/Project/updateProject";
+import UpdateProject from "./components/Project/UpdateProject";
+import ProjectBoard from "./components/ProjectBoard/ProjectBoard";
+import AddProjectTask from "./components/ProjectBoard/ProjectTasks/AddProjectTask";
 
 class App extends Component {
   render() {
@@ -19,7 +20,13 @@ class App extends Component {
             <Header />
             <Route exact path="/dashboard" component={Dashboard} />
             <Route exact path="/addProject" component={AddProject} />
-            <Route exact path="/updateProject/:id" component={updateProject} />
+            <Route exact path="/updateProject/:id" component={UpdateProject} />
+            <Route exact path="/projectBoard/:id" component={ProjectBoard} />
+            <Route
+              exact
+              path="/addProjectTask/:id"
+              component={AddProjectTask}
+            />
           </div>
         </Router>
       </Provider>
